6a8b3bc11550c538b28317502b699c2db46e2283
Giftea <eleojogiftuhiene@gmail.com>
2023-02-21 20:02:44 +0100

    Merged smaple task

--- coreLogic.js
+++ coreLogic.js
@@ -1,111 +1,173 @@
 const { namespaceWrapper } = require("./namespaceWrapper");
+const crypto = require("crypto");
 
-
+/**
+ * @description Contains the logic to do the work required
+ * for submitting the values and optionally store the result in levelDB
+ */
 async function task() {
-  // Write the logic to do the work required for submitting the values and optionally store the result in levelDB
-}
-async function fetchSubmission(){
-  // Write the logic to fetch the submission values here and return the cid string
-}
+  const x = Math.random().toString(); // generate random number and convert to string
+  const cid = crypto.createHash("sha1").update(x).digest("hex"); // convert to CID
+  console.log("HASH:", cid);
 
-async function generateDistributionList(){
-  console.log("GenerateDistributionList called");
-  console.log("I am selected node");
-
-  // Write the logic to generate the distribution list here by introducing the rules of your choice
-
-
-  /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******
-  
-  let distributionList = {};
-    const taskAccountDataJSON = await namespaceWrapper.getTaskState();
-    const submissions = taskAccountDataJSON.submissions[round];
-    const submissions_audit_trigger =
-                  taskAccountDataJSON.submissions_audit_trigger[round];
-    if (submissions == null) {
-      console.log("No submisssions found in N-2 round");
-      return distributionList;
-    } else {
-      const keys = Object.keys(submissions);
-      const values = Object.values(submissions);
-      const size = values.length;
-      console.log("Submissions from last round: ", keys, values, size);
-      for (let i = 0; i < size; i++) {
-        const candidatePublicKey = keys[i];
-        if (submissions_audit_trigger && submissions_audit_trigger[candidatePublicKey]) {
-          console.log(submissions_audit_trigger[candidatePublicKey].votes, "distributions_audit_trigger votes ");
-          const votes = submissions_audit_trigger[candidatePublicKey].votes;
-          let numOfVotes = 0;
-          for (let index = 0; index < votes.length; index++) {
-            if(votes[i].is_valid)
-              numOfVotes++;
-            else numOfVotes--;
-          }
-          if(numOfVotes < 0)
-            continue;
-        }
-        distributionList[candidatePublicKey] = 1;  
-      }
-    } */
-    
+  if (cid) {
+    await namespaceWrapper.storeSet("cid", cid); // store CID in levelDB
+  }
+  return false;
 }
 
-
-async function submitDistributionList(round) {
-  console.log("SubmitDistributionList called");
-  
-    const distributionList = await generateDistributionList();
-    
-    const decider = await namespaceWrapper.uploadDistributionList(
-      distributionList, round
-    );
-    console.log("DECIDER", decider);
-  
-    if (decider) {
-      const response = await namespaceWrapper.distributionListSubmissionOnChain(round);
-      console.log("RESPONSE FROM DISTRIBUTION LIST", response);
-    }
+/**
+ * @description Fetch's submission from levelDB
+ * @returns {string} The CID
+ */
+async function fetchSubmission() {
+  const cid = await namespaceWrapper.storeGet("cid"); // retrieves the cid
+  return cid;
 }
 
-
-async function validateNode(node) {
-  
-// Write your logic for the validation of submission value here and return a boolean value in response
-
-console.log("Validating Node", node);
-  return true;
-}
-
-async function validateDistribution(node) {
-
-// Write your logic for the validation of submission value here and return a boolean value in response
-// this logic can be same as generation of distribution list function and based on the comparision will final object , decision can be made
-  console.log("Validating Node", node);
-  return true;
-}
-// Submit Address with distributioon list to K2
+/**
+ * @description Submits a node's result to K2
+ * @param {number} roundNumber Current round number of the task
+ */
 async function submitTask(roundNumber) {
   console.log("submitTask called with round", roundNumber);
   try {
     console.log("inside try");
-    console.log(await namespaceWrapper.getSlot(), "current slot while calling submit");
+    console.log(
+      await namespaceWrapper.getSlot(),
+      "current slot while calling submit"
+    );
     const cid = await fetchSubmission();
-    await namespaceWrapper.checkSubmissionAndUpdateRound(cid, roundNumber);
+    await namespaceWrapper.checkSubmissionAndUpdateRound(cid, roundNumber); // submit to K2
     console.log("after the submission call");
   } catch (error) {
     console.log("error in submission", error);
   }
 }
 
+/**
+ * @description Contains logic for the validation of submission value
+ *
+ * @param {string} submission value
+ * @returns {boolean} The validity of the submission
+ */
+async function validateNode(node) {
+  console.log("Validating Node", node);
+  const cid = node.submission_value; // Retrieve node's submission value
+
+  const char = cid.charAt(0);
+  // If first character of cid is in the first 23 letters of the alphabet, return true
+  if (char.match(/[a-w]/i)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * @description Submits validateNode function with roundNumber
+ *
+ * @param {number} roundNumber Current round number of the task
+ */
 async function auditTask(roundNumber) {
   console.log("auditTask called with round", roundNumber);
-  console.log(await namespaceWrapper.getSlot(), "current slot while calling auditTask");
+  console.log(
+    await namespaceWrapper.getSlot(),
+    "current slot while calling auditTask"
+  );
   await namespaceWrapper.validateAndVoteOnNodes(validateNode, roundNumber);
 }
 
+/**
+ * @description Genrates a distribution list that contains the key:value pair
+ * of participating nodes public key and amount of KOII to be rewarded.
+ * Introduce preffered rules when generating a distribution list
+ * @param {number} round Current round of the task
+ */
+async function generateDistributionList(round) {
+  console.log("GenerateDistributionList called");
+  console.log("I am selected node");
+
+  let distributionList = {}; // init distribution list
+  const taskAccountDataJSON = await namespaceWrapper.getTaskState(); // init distribution list
+  const submissions = taskAccountDataJSON.submissions[round];
+  const submissions_audit_trigger =
+                taskAccountDataJSON.submissions_audit_trigger[round];
+  if (submissions == null) {
+    console.log("No submissions found in N-2 round");
+    return distributionList;
+  } else {
+    const keys = Object.keys(submissions);
+    const values = Object.values(submissions);
+    const size = values.length;
+    console.log("Submissions from last round: ", keys, values, size);
+    for (let i = 0; i < size; i++) {
+      const candidatePublicKey = keys[i];
+      if (submissions_audit_trigger && submissions_audit_trigger[candidatePublicKey]) {
+        console.log(submissions_audit_trigger[candidatePublicKey].votes, "distributions_audit_trigger votes");
+        const votes = submissions_audit_trigger[candidatePublicKey].votes;
+        let numOfVotes = 0;
+        for (let index = 0; index < votes.length; index++) {
+          if(votes[i].is_valid)
+            numOfVotes++;
+          else numOfVotes--;
+        }
+        if(numOfVotes < 0)
+          continue;
+      }
+      distributionList[candidatePublicKey] = 1;  
+    }
+  }
+}
+
+/**
+ * @description Submits distribution list to K2 including the current round
+ *
+ * @param {number} round Current round of the task
+ */
+async function submitDistributionList(round) {
+  console.log("SubmitDistributionList called");
+
+  const distributionList = await generateDistributionList(); // get distribution list
+
+  const decider = await namespaceWrapper.uploadDistributionList( 
+    distributionList,
+    round
+  );
+  console.log("DECIDER", decider);
+
+  if (decider) {
+    const response = await namespaceWrapper.distributionListSubmissionOnChain(
+      round
+    );
+    console.log("RESPONSE FROM DISTRIBUTION LIST", response);
+  }
+}
+
+/**
+ * @description Contains logic for the validation of distribution list
+ *
+ * @param {string} distributionList
+ * @returns {boolean} The validity of the distribution list
+ */
+async function validateDistribution(node) {
+  // Write your logic for the validation of submission value here and return a boolean value in response
+  // this logic can be same as generation of distribution list function and based on the comparision will final object , decision can be made
+  console.log("Validating Node", node);
+  return true;
+}
+
+/**
+ * @description Submits validateDistribution function with roundNumber
+ *
+ * @param {number} roundNumber Current round number of the task
+ */
 async function auditDistribution(roundNumber) {
   console.log("auditDistribution called with round", roundNumber);
-  await namespaceWrapper.validateAndVoteOnDistributionList(validateDistribution, roundNumber);
+  await namespaceWrapper.validateAndVoteOnDistributionList(
+    validateDistribution,
+    roundNumber
+  );
 }
 
 module.exports = {
@@ -115,5 +177,5 @@ module.exports = {
   validateDistribution,
   submitTask,
   auditTask,
-  auditDistribution
+  auditDistribution,
 };
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const coreLogic = require("./coreLogic");
 const { app } = require("./init");
 const { namespaceWrapper } = require("./namespaceWrapper");
 
-
 async function setup() {
   console.log("setup function called");
   // Run default setup
@@ -15,93 +14,17 @@ async function setup() {
     } else if (m.functionCall == "auditPayload") {
       console.log("auditPayload called");
       coreLogic.auditTask(m.roundNumber);
-    }
-    else if(m.functionCall == "executeTask") {
+    } else if (m.functionCall == "executeTask") {
       console.log("executeTask called");
       coreLogic.task();
-    }
-    else if(m.functionCall == "generateAndSubmitDistributionList") {
+    } else if (m.functionCall == "generateAndSubmitDistributionList") {
       console.log("generateAndSubmitDistributionList called");
       coreLogic.submitDistributionList(m.roundNumber);
-    }
-    else if(m.functionCall == "distributionListAudit") {
+    } else if (m.functionCall == "distributionListAudit") {
       console.log("distributionListAudit called");
       coreLogic.auditDistribution(m.roundNumber);
     }
   });
-
-
-  /* GUIDE TO CALLS K2 FUNCTIONS MANUALLY
-
-  If you wish to do the development by avoiding the timers then you can do the intended calls to K2 
-  directly using these function calls. 
-
-  To disable timers please set the TIMERS flag in task-node ENV to disable
-
-  NOTE : K2 will still have the windows to accept the submission value, audit, so you are expected
-  to make calls in the intended slots of your round time. 
-
-
-  // Get the task state 
-  console.log(await namespaceWrapper.getTaskState());
-
-  //GET ROUND 
-
-  const round = await namespaceWrapper.getRound();
-  console.log("ROUND", round);
-
-  // Submission to K2 (Preferablly you should submit the cid received from IPFS)
-
-  await namespaceWrapper.checkSubmissionAndUpdateRound("vjnkjbvbvhj87847" [PASS YOUR ROUND HERE]);
-
-
-  // Audit submissions 
-
-  await namespaceWrapper.validateAndVoteOnNodes(validateNode, [PASS YOUR ROUND HERE]);
-
-
-  // Node selection for distribution list 
-
-  const selectedNode = await namespaceWrapper.nodeSelectionDistributionList();
-  console.log("SELECTED NODE", selectedNode);
-
-  // Distribution list sample 
-
-  const distributionList = {
-    "29SSj6EQARvATESSQbBcmSE3A1iaWwqXFunzAEDoV7Xj": 4,
-    "3KUfsjpjCSCjwCBm4TraM5cGx6YzEUo9rrq2hrSsJw3x": 5,
-    Avxvdc2efsPqysBZt4VKDSgiP4iuJ8GaAWhsNVUAi5CZ: 6,
-  };
-
-  // upload distribution list to K2
-
-  const decider = await namespaceWrapper.uploadDistributionList(
-    distributionList
-    [PASS YOUR ROUND HERE]
-  );
-  console.log("DECIDER", decider);
-
-  // Do the submission only if the distribution was uploaded correctly based on decider 
-
-  if (decider) {
-    const response = await namespaceWrapper.distributionListSubmissionOnChain();
-    console.log("RESPONSE FROM DISTRIBUTION LIST", response);
-  }
-
-  // Audit distribution list
-
-  await namespaceWrapper.validateAndVoteOnDistributionList();
-
-  // Payout trigger
-
-  const responsePayout = await namespaceWrapper.payoutTrigger();
-  console.log("RESPONSE TRIGGER", responsePayout);
-
-
-
-  */
-
-
 }
 
 setup();
@@ -111,4 +34,3 @@ if (app) {
   //  For Example
   //  namespace.express('post', '/accept-cid', async (req, res) => {})
 }
-
