f205706f7fffba76698766d97badf21099d4c4fe
Raj Prajapati <rajce604@gmail.com>
2023-03-01 09:21:33 -0500

    added manual calls

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,47 @@ async function setup() {
 
 setup();
 
+/* GUIDE TO CALLS K2 FUNCTIONS MANUALLY
+
+  If you wish to do the development by avoiding the timers then you can do the intended calls to K2 
+  directly using these function calls. 
+
+  To disable timers please set the TIMERS flag in task-node ENV to disable
+
+  NOTE : K2 will still have the windows to accept the submission value, audit, so you are expected
+  to make calls in the intended slots of your round time. 
+
+  */
+
+// Get the task state
+//console.log(await namespaceWrapper.getTaskState());
+
+//GET ROUND
+
+// const round = await namespaceWrapper.getRound();
+// console.log("ROUND", round);
+
+// Submission to K2 (Preferablly you should submit the cid received from IPFS)
+
+//await submitTask(round - 1)
+
+// Audit submissions
+
+//await auditTask(round - 1)
+
+// upload distribution list to K2
+
+//await submitDistributionList(round - 2)
+
+// Audit distribution list
+
+//await auditDistribution(round - 2);
+
+// Payout trigger
+
+// const responsePayout = await namespaceWrapper.payoutTrigger();
+// console.log("RESPONSE TRIGGER", responsePayout);
+
 if (app) {
   //  Write your Express Endpoints here.
   //  For Example
