699d18ee65c4a6daa8611d2a132ab6460f9f357e
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2023-03-30 01:53:05 +0500

    Unit tests completed for the task template

--- tests/unitTest.js
+++ tests/unitTest.js
@@ -1,4 +1,5 @@
-const coreLogic=require("../coreLogic")
-const index = require("../index")
-coreLogic.task()
-coreLogic.fetchSubmission()
+const coreLogic = require('../coreLogic');
+const index = require('../index');
+coreLogic.task();
+const submission = coreLogic.fetchSubmission();
+coreLogic.validateNode(submission, 1);
