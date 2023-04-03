acfacc62606773d7432c3f8cbad7b70497983964
soma <soma@koii.network>
2023-04-03 20:07:32 -0300

    remove soma's web3 token key

--- linktree_task.js
+++ linktree_task.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const { Web3Storage, getFilesFromPath } = require('web3.storage');
 const storageClient = new Web3Storage({
   token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGY0ODYxMzAzOTdDNTY1QzlDYTRCOTUzZTA2RWQ4NUI4MGRBQzRkYTIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjYzNjU1OTk5MDMsIm5hbWUiOiJTb21hIn0.TU-KUFS9vjI9blN5dx6VsLLuIjJnpjPrxDHBvjXQUxw',
+    '',
 });
 const bs58 = require('bs58');
 const nacl = require('tweetnacl');
