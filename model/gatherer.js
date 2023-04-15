bb675c453a6a7bd6d424da77d767c908177ba173
alexander-morris <al>
2023-04-15 14:27:03 -0300

    fixing minor interface issues

--- model/gatherer.js
+++ model/gatherer.js
@@ -1,6 +1,6 @@
 const Adapter = require('./adapter');
 const Data = require('./data');
-const 
+const Search = require('./search');
 
 class Gatherer {
     constructor(db, dataSrc, credentials, maxRetry, shims, startQuery, maxdepth, maxItems, maxItemsPerList, maxLists) {
@@ -19,12 +19,29 @@ class Gatherer {
 
     gather( ) {
         // set up an adapter
+        // authenticate with the adapter
+        let session = this.adapter.negotiateSession();
 
         // generate a new search
-
+        let search = new Search(this.options.startQuery, this.options, this.adapter);
+        
         // parse items and crawl links for more items
+        let items = this.search.getList();
+
+        // save items to the db
+        for (const item of items) {
+
+            this.db.create(items);
+
+        }
+
+        // save search to the db
+        this.db.create(search);
 
     }
+
+    // TODO - fix the methods below with proper db prefix mgmt
+    // TODO then integrate them into the above flows to allow async queueing of reads from APIs and writes to the db
     getData(id) {
         return this.db.get(id);
     }
--- model/search.js
+++ model/search.js
@@ -1,20 +1,29 @@
 
 class Search {
-    constructor(query) {
+    constructor(query, options, adapter) {
         console.log(query)
-        this.offset = 0;
-        this.adapter = adapter; // holds the adapter object, including getList, parseOne
-    }
-    setQuery(query) { 
-        // manages getting and setting the query for the search queue
         this.query = query;
+        this.options = options;
+        this.options.offset = options.offset || 0;
+        this.adapter = adapter; // holds the adapter object, including getList, parseOne
+        this.list = []; // holds the list of items returned by the search
+        this.lists = []; // holds the list of lists returned by the search
     }
+
     setOffset(offset) {
         // manages getting and setting the offset for the search queue
-        this.offset = offset;
+        this.options.offset = offset;
     }
     getNext() {
         // TODO - provides a basic iterator for the search queue
+        // i.e.
+        let minOffset = this.options.offsetSize || 0;
+        if (minOffset == 0) { 
+            return console.error('ERROR: Tried to trigger getNext() for a search, but no offsetSize provided."', `Query ${ this.query }`)
+        } else {
+            return new Search ( this.query, { offset: this.options.offset + minOffset }, this.adapter );
+        }
+            
     }
 
     
