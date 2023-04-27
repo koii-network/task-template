0f46e77e974d83bdeb43e3ad7449acc465dfa06d
soma <soma@koii.network>
2023-04-27 10:35:09 -0300

    use puppeteer log in to twitter

---
+++ tests/test_twitter_call.js
@@ -0,0 +1,62 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const username = 'soma@koii.network';
+const username_check = 'Soma41717079';
+const password = '970128ldydq';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.on('console', consoleObj => console.log(consoleObj.text()));
+  await page.setViewport({ width: 1280, height: 800 });
+
+  console.log('Step: Reach twitter login page');
+
+  await page.goto('https://twitter.com/i/flow/login');
+  // Wait an additional 5 seconds before scraping
+  await page.waitForTimeout(5000);
+
+  console.log('Step: Fill in username');
+
+  await page.waitForSelector('input[autocomplete="username"]');
+  await page.type('input[autocomplete="username"]', username);
+  await page.keyboard.press('Enter');
+
+  if (
+    await page.waitForSelector('input[data-testid="ocfEnterTextTextInput"]')
+  ) {
+    await page.type(
+      'input[data-testid="ocfEnterTextTextInput"]',
+      username_check,
+    );
+    await page.keyboard.press('Enter');
+  }
+
+  console.log('Step: Fill in password');
+
+  await page.waitForSelector('input[name="password"]');
+  await page.type('input[name="password"]', password);
+  await page.keyboard.press('Enter');
+
+  console.log('Step: Click login button');
+
+  page.waitForNavigation({ waitUntil: 'load' });
+
+  console.log('Step: Login successful');
+
+  // Wait an additional 5 seconds until fully log in 
+  await page.waitForTimeout(5000);
+  await page.goto('https://twitter.com/search?q=%23Web3&src=typed_query');
+
+  // Wait an additional 5 seconds until fully loaded before scraping
+  await page.waitForTimeout(5000);
+  const html = await page.content();
+  const $ = cheerio.load(html);
+
+  $('div[data-testid="tweet"]').each((i, el) => {
+    const tweet = $(el).find('div[lang]').text();
+    console.log(tweet);
+  });
+})();
