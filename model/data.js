d353405b8fb108d25a68b34a0d3793381afa92f2
soma <soma@koii.network>
2023-05-02 12:26:39 -0300

    update leveldb class

--- model/data.js
+++ model/data.js
@@ -1,164 +1,183 @@
 const levelup = require('levelup');
 const leveldown = require('leveldown');
-const Item = require(__dirname + '/item');
+const Item = require('./item');
 
 class Data {
-    constructor (name, db, data) {
-        this.name = name;
-        this.db = db;
-        this.dbprefix = `${name} + ":"`;
-        if (data) data.forEach((item) => {
-            this.create(item)
-            console.log(`Created ${this.fullList.length} new items from a list of ${ data.length}`)
-        })
-        this.fullList = this.getList();
-        this.lastUpdate = Date.now();
+  constructor(name, db, data) {
+    this.name = name;
+    this.db = db;
+    this.dbprefix = `${name} + ":"`;
+    this.fullList = [];
+    this.lastUpdate = Date.now();
+  }
+
+  // creates new database with received data
+  async createItems(data) {
+    for (let i = 0; i < data.length; i++) {
+        try {
+            const createdItem = await this.create(data[i]);
+            this.fullList.push(createdItem);
+            console.log(`Created ${this.fullList.length} items from a list of ${data.length}`);
+          } catch (err) {
+            console.error('Error creating item:', err);
+        }
     }
+}
+  // returns items by id
+  async get(id) {
+    return new Promise((resolve, reject) => {
+      this.db.get(this.createId(id), (err, value) => {
+        if (err) {
+          // console.error("Error in getData get", err, id);
+          resolve(null);
+        } else {
+          resolve(JSON.parse(value || '[]'));
+        }
+      });
+    });
+  }
 
-    // returns items by id
-    get (id) {
-        return new Promise((resolve, reject) => {
-            this.db.get( this.createId(id), (err, value) => {
-              if (err) {
-                // console.error("Error in getData get", err, id);
-                resolve(null);
-              } else {
-                resolve(JSON.parse(value || "[]"));
-              }
-            });
-        });
-    }   
+  addPendingItem(id, value) {
+    return new Promise((resolve, reject) => {
+      this.db.put(this.createPendingId(id), JSON.stringify(value), err => {
+        if (err) {
+          console.error('Error in addPendingItem', err);
+          reject(err);
+        } else {
+          console.log('added pending item', id);
+          resolve(true);
+        }
+      });
+    });
+  }
 
-    addPendingItem(id, value) {
-        return new Promise((resolve, reject) => {
-            this.db.put( this.createPendingId(id), JSON.stringify(value), (err) => {
-                if (err) {
-                    console.error("Error in addPendingItem", err);
-                    reject(err);
-                } else {
-                    console.log('added pending item', id)
-                    resolve(true);
-                }
-            });
-        });
-    }
+  getPending(limit) {
+    return new Promise((resolve, reject) => {
+      let dataStore = [];
+      let options = {
+        gt: `~pending:${this.name}~`,
+        reverse: true,
+        keys: true,
+        values: true,
+      };
+      if (limit) options.limit = limit;
 
-    getPending(limit) {
+      this.db
+        .createReadStream(options)
+        .on('data', function (data) {
+          console.log(data.key.toString(), '=', data.value.toString());
+          dataStore.push(JSON.parse(data.value.toString()));
 
-        return new Promise((resolve, reject) => {
-            let dataStore = [];
-            let options = {
-              gt: `~pending:${ this.name }~`,
-              reverse: true,
-              keys: true,
-              values: true
+          // check if the limit has been reached
+          if (limit && dataStore.length >= limit) {
+            console.log('limit reached');
+            this.destroy(); // TODO - test this
           }
-          if (limit) options.limit = limit;
-
-          this.db.createReadStream(options)
-            .on('data', function (data) {
-                console.log( data.key.toString(), '=', data.value.toString())
-                dataStore.push(JSON.parse(data.value.toString()));
-
-                // check if the limit has been reached
-                if (limit && dataStore.length >= limit) {
-                    console.log('limit reached')
-                    this.destroy();  // TODO - test this
-                }
-
-            })
-            .on('error', function (err) {
-                console.log('Oh my!', err)
-                reject(err);
-            })
-            .on('close', function () {
-                console.log('Stream closed')
-            })
-            .on('end', function () {
-                console.log('Stream ended')
-                resolve(dataStore);
-            })  
+        })
+        .on('error', function (err) {
+          console.log('Oh my!', err);
+          reject(err);
+        })
+        .on('close', function () {
+          console.log('Stream closed');
+        })
+        .on('end', function () {
+          console.log('Stream ended');
+          resolve(dataStore);
         });
-    }
+    });
+  }
 
-    isPendingItem (id) {
-        return new Promise((resolve, reject) => {
-          this.db.get( this.createPendingId(id), (err, value) => {
-              if (err) {
-                console.error("Error in getData get", err, id);
-                resolve(null);
-              } else {
-                resolve(JSON.parse(value || "[]"));
-              }
-            });
+  isPendingItem(id) {
+    return new Promise((resolve, reject) => {
+      this.db.get(this.createPendingId(id), (err, value) => {
+        if (err) {
+          console.error('Error in getData get', err, id);
+          resolve(null);
+        } else {
+          resolve(JSON.parse(value || '[]'));
+        }
+      });
+    });
+  }
+
+  isDataItem(id) {
+    return new Promise((resolve, reject) => {
+      this.get(this.createId(id), (err, value) => {
+        if (err) {
+          console.error('Error in getData - dataItem ', err, id);
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  getList(options) {
+    // if no options supplied, default to a list of stored items by their keys
+    if (!options)
+      options = {
+        lt: `${this.name}~`,
+        reverse: true,
+        keys: true,
+        values: false,
+      };
+    return new Promise((resolve, reject) => {
+      let dataStore = [];
+      this.db
+        .createReadStream(options)
+        .on('data', function (data) {
+          console.log(data.key.toString(), '=', data.value.toString());
+          dataStore.push({
+            key: data.key.toString(),
+            value: JSON.parse(data.value.toString()),
+          });
+        })
+        // TODO - add error handling
+        .on('error', function (err) {
+          console.log('Something went wrong in read linktreesStream!', err);
+          reject(err);
+        })
+        .on('close', function () {
+          console.log('Stream closed');
+        })
+        .on('end', function () {
+          console.log('Stream ended');
+          resolve(dataStore);
         });
-    }
+    });
+  }
 
-    isDataItem (id) {
-        return new Promise((resolve, reject) => {
-            this.get( this.createId(id), (err, value) => {
-              if (err) {
-                console.error("Error in getData - dataItem ", err, id);
-                resolve(false);
-              } else {
-                resolve(true);
-              }
-            });
-        });
-    }
+  // adds a new item
+  create(item) {
+    return new Promise((resolve, reject) => {
+      this.db.put(this.createId(item.id), JSON.stringify(item), err => {
+        if (err) {
+          console.error('Error in create', err);
+          reject(err);
+        } else {
+          console.log('Created item', item.id);
+          resolve(item);
+        }
+      });
+    });
+  }
+  createId(id) {
+    console.log('Received DB name is', this.name);
+    let newId = `${this.name}:${id}`;
+    console.log('new id is ', newId);
+    return newId;
+  }
 
-    getList(options) {
-        // if no options supplied, default to a list of stored items by their keys
-        if (!options) options = {
-            lt: `${ this.name }~`,
-            reverse: true,
-            keys: true,
-            values: false
-          };
-        return new Promise((resolve, reject) => {
-            let dataStore = [];
-            this.db.createReadStream(options)
-              .on('data', function (data) {
-                console.log( data.key.toString(), '=', data.value.toString())
-                dataStore.push({ key: data.key.toString(), value: JSON.parse(data.value.toString()) });
-              })
-              // TODO - add error handling
-              .on('error', function (err) {
-                console.log('Something went wrong in read linktreesStream!', err);
-                reject(err);
-              })
-              .on('close', function () {
-                console.log('Stream closed')
-              })
-              .on('end', function () {
-                console.log('Stream ended')
-                resolve(dataStore);
-              })
-        });
-    }
-
-    // adds a new item 
-    create (item) {
-        item = new Item(item); // item must be an instance of Item
-        return db.put( this.createId(itemId), JSON.stringify(item));
-    }  
-    createId (id) {
-      console.log('this.name', this.name)
-      let newId = `${ this.name }:${ id }`;
-      console.log('new id is ', newId)
-      return newId;
-    }
-  
-    createPendingId (id) {
-      console.log(id)
-      let normalId = this.createId(id);
-      console.log('normal ID is ' + normalId);
-      let pendingId = `pending:${ normalId }`;
-      console.log('new pending ID: ', pendingId)
-      return pendingId;
-    }
-
+  createPendingId(id) {
+    console.log(id);
+    let normalId = this.createId(id);
+    console.log('normal ID is ' + normalId);
+    let pendingId = `pending:${normalId}`;
+    console.log('new pending ID: ', pendingId);
+    return pendingId;
+  }
 }
 
 module.exports = Data;
-
---
+++ tests/test_data.js
@@ -0,0 +1,81 @@
+const assert = require('assert');
+const levelup = require('levelup');
+const leveldown = require('leveldown');
+const Data = require('../model/data');
+const Item = require('./test_item');
+
+const db = levelup(leveldown('./my_test_db'));
+
+const testData = [
+  new Item({ id: 1, name: 'Item 1', description: 'First item' }),
+  new Item({ id: 2, name: 'Item 2', description: 'Second item' }),
+//   new Item({ id: 3, name: 'Item 3', description: 'Third item' }),
+];
+
+console.log('Running tests...', testData);
+
+const data = new Data('test', db, testData);
+
+// Test creating an item
+data.createItems(testData)
+  .then(() => {
+    console.log('Create item test passed');
+  })
+  .catch((err) => {
+    console.error('Create item test failed:', err);
+  });
+
+
+
+// Test creating an item
+data.create(new Item({ id: 4, name: 'Item 4', description: 'Fourth item' }))
+  .then(() => {
+    console.log('Create item test passed');
+  })
+  .catch((err) => {
+    console.error('Create item test failed:', err);
+  });
+
+// // Test retrieving an item
+// data.get(1)
+//   .then((item) => {
+//     assert.deepStrictEqual(item, testData[0]);
+//     console.log('Get item test passed');
+//   })
+//   .catch((err) => {
+//     console.error('Get item test failed:', err);
+//   });
+
+// // Test getting a list of items
+// data.getList()
+//   .then((list) => {
+//     assert.strictEqual(list.length, testData.length);
+//     console.log('Get list test passed');
+//   })
+//   .catch((err) => {
+//     console.error('Get list test failed:', err);
+//   });
+
+// // Test pending item functionality
+// data.addPendingItem(5, new Item({ id: 5, name: 'Item 5', description: 'Fifth item' }))
+//   .then(() => {
+//     console.log('Add pending item test passed');
+//   })
+//   .catch((err) => {
+//     console.error('Add pending item test failed:', err);
+//   });
+
+// // Test getting pending items
+// data.getPending()
+//   .then((list) => {
+//     assert.strictEqual(list.length, 1);
+//     console.log('Get pending items test passed');
+//   })
+//   .catch((err) => {
+//     console.error('Get pending items test failed:', err);
+//   });
+
+// // Clean up the test database
+// db.close(() => {
+//   console.log('Database closed');
+// });
