2cda40401b630610860ac648ed342bca6fba7375
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2023-06-15 00:30:54 +0500

    Added endpoint test

--- tests/main.test.js
+++ tests/main.test.js
@@ -2,7 +2,7 @@ const { coreLogic } = require('../coreLogic');
 const { _server } = require('../init');
 const { namespaceWrapper } = require('../namespaceWrapper');
 const Joi = require('joi');
-
+const axios = require('axios');
 beforeAll(async () => {
   await namespaceWrapper.defaultTaskSetup();
 });
@@ -22,22 +22,25 @@ describe('Performing the task', () => {
     const round = 1;
     await coreLogic.submitTask(round);
     const taskState = await namespaceWrapper.getTaskState();
-    const schema = Joi.object().pattern(
-      Joi.string(),
-      Joi.object().pattern(
+    const schema = Joi.object()
+      .pattern(
         Joi.string(),
-        Joi.object({
-          submission_value: Joi.string().required(),
-          slot: Joi.number().integer().required(),
-          round: Joi.number().integer().required(),
-        })
+        Joi.object().pattern(
+          Joi.string(),
+          Joi.object({
+            submission_value: Joi.string().required(),
+            slot: Joi.number().integer().required(),
+            round: Joi.number().integer().required(),
+          }),
+        ),
       )
-    ).required().min(1);
+      .required()
+      .min(1);
     const validationResult = schema.validate(taskState.submissions);
-    try{
+    try {
       expect(validationResult.error).toBeUndefined();
-    }catch(e){
-      throw new Error("Submission doesn't exist or is incorrect")
+    } catch (e) {
+      throw new Error("Submission doesn't exist or is incorrect");
     }
   });
 
@@ -45,89 +48,114 @@ describe('Performing the task', () => {
     const round = 1;
     await coreLogic.auditTask(round);
     const taskState = await namespaceWrapper.getTaskState();
-    console.log("audit task",taskState.submissions_audit_trigger)
-    const schema = Joi.object().pattern(
-      Joi.string(),
-      Joi.object().pattern(
+    console.log('audit task', taskState.submissions_audit_trigger);
+    const schema = Joi.object()
+      .pattern(
         Joi.string(),
-        Joi.object({
-          trigger_by: Joi.string().required(),
-          slot: Joi.number().integer().required(),
-          votes: Joi.array().required(),
-        })
+        Joi.object().pattern(
+          Joi.string(),
+          Joi.object({
+            trigger_by: Joi.string().required(),
+            slot: Joi.number().integer().required(),
+            votes: Joi.array().required(),
+          }),
+        ),
       )
-    ).required();
-    const validationResult = schema.validate(taskState.submissions_audit_trigger);
-    try{
+      .required();
+    const validationResult = schema.validate(
+      taskState.submissions_audit_trigger,
+    );
+    try {
       expect(validationResult.error).toBeUndefined();
-    }catch(e){
-      throw new Error("Submission audit is incorrect")
+    } catch (e) {
+      throw new Error('Submission audit is incorrect');
     }
   });
   it('should make the distribution submission to k2 for dummy round 1', async () => {
     const round = 1;
     await coreLogic.submitDistributionList(round);
     const taskState = await namespaceWrapper.getTaskState();
-    const schema = Joi.object().pattern(
-      Joi.string(),
-      Joi.object().pattern(
+    const schema = Joi.object()
+      .pattern(
         Joi.string(),
-        Joi.object({
-          submission_value: Joi.string().required(),
-          slot: Joi.number().integer().required(),
-          round: Joi.number().integer().required(),
-        })
+        Joi.object().pattern(
+          Joi.string(),
+          Joi.object({
+            submission_value: Joi.string().required(),
+            slot: Joi.number().integer().required(),
+            round: Joi.number().integer().required(),
+          }),
+        ),
       )
-    ).required().min(1);
-    console.log(taskState.distribution_rewards_submission)
-    const validationResult = schema.validate(taskState.distribution_rewards_submission);
-    try{
+      .required()
+      .min(1);
+    console.log(taskState.distribution_rewards_submission);
+    const validationResult = schema.validate(
+      taskState.distribution_rewards_submission,
+    );
+    try {
       expect(validationResult.error).toBeUndefined();
-    }catch(e){
-      throw new Error("Distribution submission doesn't exist or is incorrect")
+    } catch (e) {
+      throw new Error("Distribution submission doesn't exist or is incorrect");
     }
   });
   it('should make the make an audit on distribution submission', async () => {
     const round = 1;
     await coreLogic.auditDistribution(round);
     const taskState = await namespaceWrapper.getTaskState();
-    console.log("audit task",taskState.distributions_audit_trigger)
-    const schema = Joi.object().pattern(
-      Joi.string(),
-      Joi.object().pattern(
+    console.log('audit task', taskState.distributions_audit_trigger);
+    const schema = Joi.object()
+      .pattern(
         Joi.string(),
-        Joi.object({
-          trigger_by: Joi.string().required(),
-          slot: Joi.number().integer().required(),
-          votes: Joi.array().required(),
-        })
+        Joi.object().pattern(
+          Joi.string(),
+          Joi.object({
+            trigger_by: Joi.string().required(),
+            slot: Joi.number().integer().required(),
+            votes: Joi.array().required(),
+          }),
+        ),
       )
-    ).required();
-    const validationResult = schema.validate(taskState.distributions_audit_trigger);
-    try{
+      .required();
+    const validationResult = schema.validate(
+      taskState.distributions_audit_trigger,
+    );
+    try {
       expect(validationResult.error).toBeUndefined();
-    }catch(e){
-      throw new Error("Distribution audit is incorrect")
+    } catch (e) {
+      throw new Error('Distribution audit is incorrect');
     }
   });
 
   it('should make sure the submitted distribution list is valid', async () => {
     const round = 1;
-    const distributionList = await namespaceWrapper.getDistributionList(null,round);
-    console.log("Generated distribution List",JSON.parse(distributionList.toString()))
-    const schema = Joi.object().pattern(
-      Joi.string().required(),
-      Joi.number().integer().required()
-      ).required();
-    const validationResult = schema.validate(JSON.parse(distributionList.toString()));
-    console.log(validationResult)
-    try{
+    const distributionList = await namespaceWrapper.getDistributionList(
+      null,
+      round,
+    );
+    console.log(
+      'Generated distribution List',
+      JSON.parse(distributionList.toString()),
+    );
+    const schema = Joi.object()
+      .pattern(Joi.string().required(), Joi.number().integer().required())
+      .required();
+    const validationResult = schema.validate(
+      JSON.parse(distributionList.toString()),
+    );
+    console.log(validationResult);
+    try {
       expect(validationResult.error).toBeUndefined();
-    }catch(e){
-      throw new Error("Submitted distribution list is not valid")
+    } catch (e) {
+      throw new Error('Submitted distribution list is not valid');
     }
   });
 
+  it('should test the endpoint', async () => {
+    const response = await axios.get('http://localhost:10000');
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual("Hello World!");
+  });
 });
 
 afterAll(async () => {
