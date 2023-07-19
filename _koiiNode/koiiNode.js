69985d6f78b14aa2dd749f06525ba0409a481829
Syed Ghazanfer Anwar <s.ghazanfer@koii.network>
2023-07-19 14:11:39 +0000

    Resolve "ASAP: Add default values to index.js"

--- _koiiNode/koiiNode.js
+++ _koiiNode/koiiNode.js
@@ -13,16 +13,46 @@ const express = require('express');
 // Only used for testing purposes, in production the env will be injected by tasknode
 require('dotenv').config();
 const bodyParser = require('body-parser');
-
+/**
+ * This will be the name of the current task as coming from the task node running this task.
+ */
 const TASK_NAME = process.argv[2] || 'Local';
+/** 
+ * This will be the id of the current task as coming from the task node running this task.
+ */
 const TASK_ID = process.argv[3];
+/**
+ * This will be the PORT on which the this task is expected to run the express server coming from the task node running this task. 
+ * As all communication via the task node and this task will be done on this port.
+ */
 const EXPRESS_PORT = process.argv[4] || 10000;
-const NODE_MODE = process.argv[5];
+
+// Not used anymore
+// const NODE_MODE = process.argv[5];
+
+/**
+ * This will be the main account public key in string format of the task node running this task.
+ */
 const MAIN_ACCOUNT_PUBKEY = process.argv[6];
+/**
+  * This will be the secret used by the task to authenticate with task node running this task.
+ */
 const SECRET_KEY = process.argv[7];
-const K2_NODE_URL = process.argv[8];
+/**
+ * This will be K2 url being used by the task node, possible values are 'https://k2-testnet.koii.live' | 'https://k2-devnet.koii.live' | 'http://localhost:8899'
+ */
+const K2_NODE_URL = process.argv[8] || 'https://k2-testnet.koii.live';
+/**
+ * This will be public task node endpoint (Or local if it doesn't have any) of the task node running this task.
+ */
 const SERVICE_URL = process.argv[9];
+/**
+ * This will be stake of the task node running this task, can be double checked with the task state and staking public key.
+ */
 const STAKE = Number(process.argv[10]);
+/**
+ * This will be the port used by task node as the express server port, so it can be used by the task for the communication with the task node
+ */
 const TASK_NODE_PORT = Number(process.argv[11]);
 
 const app = express();
@@ -800,15 +830,14 @@ if (taskNodeAdministered) {
 }
 module.exports = {
   namespaceWrapper,
-  taskNodeAdministered,
-  app,
-  NODE_MODE,
-  TASK_ID,
-  MAIN_ACCOUNT_PUBKEY,
-  SECRET_KEY,
-  K2_NODE_URL,
-  SERVICE_URL,
-  STAKE,
-  TASK_NODE_PORT,
-  _server,
+  taskNodeAdministered, // Boolean flag indicating that the task is being ran in active mode (Task node supervised), or development (testing) mode 
+  app, // The initialized express app to be used to register endpoints
+  TASK_ID, // This will be the PORT on which the this task is expected to run the express server coming from the task node running this task. As all communication via the task node and this task will be done on this port.
+  MAIN_ACCOUNT_PUBKEY, // This will be the secret used to authenticate with task node running this task.
+  SECRET_KEY, // This will be the secret used by the task to authenticate with task node running this task.
+  K2_NODE_URL, // This will be K2 url being used by the task node, possible values are 'https://k2-testnet.koii.live' | 'https://k2-devnet.koii.live' | 'http://localhost:8899'
+  SERVICE_URL, // This will be public task node endpoint (Or local if it doesn't have any) of the task node running this task.
+  STAKE, // This will be stake of the task node running this task, can be double checked with the task state and staking public key.
+  TASK_NODE_PORT, // This will be the port used by task node as the express server port, so it can be used by the task for the communication with the task node
+  _server, // Express server object
 };
