01d470b663330bf67cb0e54d2c12d698052c0264
Soma Li <soma@koii.network>
2024-04-16 19:14:40 +0000

    easier ENV easier life

--- prod-debug.js
+++ prod-debug.js
@@ -4,14 +4,6 @@ require('dotenv').config();
 const Debugger = require('./debugger');
 const Tail = require('tail').Tail;
 
-const debugConfig = Debugger.getConfig();
-// TODO - add switch for operating system to get Node DIR
-const nodeDIR = debugConfig.nodeDir;
-const sourcePath = __dirname + '/' + debugConfig.webpackedFilePath;
-const desktopNodeExecutablePath = nodeDIR + debugConfig.destinationPath;
-const desktopNodeLogPath = nodeDIR + debugConfig.logPath;
-const keywords = debugConfig.keywords;
-
 /*
     This script is used to watch for file changes in the project and trigger a build and copy the webpacked file to the Desktop Node runtime folder.
     It also tails the logs for messages containing a keyword specified in the .env file.
@@ -55,6 +47,15 @@ const build = async () => {
 
 /* copy the task to the Desktop Node runtime folder */
 const copyWebpackedFile = async () => {
+  const debugConfig = await Debugger.getConfig();
+  console.log('debugConfig', debugConfig);
+  const nodeDIR = debugConfig.nodeDir;
+  const sourcePath = __dirname + '/' + debugConfig.webpackedFilePath;
+  const desktopNodeExecutablePath = nodeDIR + '/' + debugConfig.destinationPath;
+  const desktopNodeLogPath = nodeDIR + '/' + debugConfig.logPath;
+  const keywords = debugConfig.keywords;
+  const taskID = debugConfig.taskID;
+
   if (!sourcePath || !desktopNodeExecutablePath) {
     console.error('Source path or destination path not specified in .env');
     return;
@@ -69,18 +70,35 @@ const copyWebpackedFile = async () => {
       console.error('Error copying file:', err);
     } else {
       console.log('File copied successfully');
-      tailLogs();
+      tailLogs(desktopNodeLogPath, keywords, taskID);
     }
   });
 };
 
 /* tail logs */
-const tailLogs = async () => {
+const tailLogs = async (desktopNodeLogPath, keywords, taskID) => {
   console.log('Watchings logs for messages containing ', keywords);
+  
+  // Ensure the log file exists, or create it if it doesn't
+  try {
+    await fs.promises.access(desktopNodeLogPath, fs.constants.F_OK);
+  } catch (err) {
+    console.log(`Log file not found, creating ${desktopNodeLogPath}`);
+    await fs.promises.writeFile(desktopNodeLogPath, '', { flag: 'a' }); // 'a' flag ensures the file is created if it doesn't exist and not overwritten if it exists
+  }
+
   let tail = new Tail(desktopNodeLogPath, '\n', {}, true);
 
+  console.warn(
+    'Now watching logs for messages containing ',
+    keywords,
+    'Please start the Task',
+    taskID,
+    ' and keep it running on the Desktop Node.',
+  );
+
   tail.on('line', function (data) {
-    // console.log(data);
+    console.log(data);
     if (keywords.some(keyword => data.includes(keyword))) {
       console.log(`PROD$ ${data}`);
     }
