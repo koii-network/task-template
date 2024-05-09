73f6a8eaeaf9881375c91c50cca2c4d77f629a99
Laura Abro <laura@koii.network>
2024-05-09 12:23:44 -0300

    add try catch to validateAndVoteOnDistributionList

--- _koiiNode/koiiNode.js
+++ _koiiNode/koiiNode.js
@@ -715,71 +715,84 @@ class NamespaceWrapper {
   }
 
   async validateAndVoteOnDistributionList(validateDistribution, round) {
-    // await this.checkVoteStatus();
-    console.log('******/  IN VOTING OF DISTRIBUTION LIST /******');
-    const taskAccountDataJSON = await this.getTaskState();
-    console.log(
-      `Fetching the Distribution submissions of round ${round}`,
-      taskAccountDataJSON.distribution_rewards_submission[round],
-    );
-    const submissions =
-      taskAccountDataJSON?.distribution_rewards_submission[round];
-    if (
-      submissions == null ||
-      submissions == undefined ||
-      submissions.length == 0
-    ) {
-      console.log(`No submisssions found in round ${round}`);
-      return `No submisssions found in round ${round}`;
-    } else {
-      const keys = Object.keys(submissions);
-      const values = Object.values(submissions);
-      const size = values.length;
+    try {    // await this.checkVoteStatus();
+      console.log('******/  IN VOTING OF DISTRIBUTION LIST /******');
+      const taskAccountDataJSON = await this.getTaskState();
       console.log(
-        'Distribution Submissions from last round: ',
-        keys,
-        values,
-        size,
+        `Fetching the Distribution submissions of round ${round}`,
+        taskAccountDataJSON.distribution_rewards_submission[round],
       );
-      let isValid;
-      const submitterAccountKeyPair = await this.getSubmitterAccount();
-      const submitterPubkey = submitterAccountKeyPair.publicKey.toBase58();
+      const submissions =
+        taskAccountDataJSON?.distribution_rewards_submission[round];
+      if (
+        submissions == null ||
+        submissions == undefined ||
+        submissions.length == 0
+      ) {
+        console.log(`No submisssions found in round ${round}`);
+        return `No submisssions found in round ${round}`;
+      } else {
+        const keys = Object.keys(submissions);
+        const values = Object.values(submissions);
+        const size = values.length;
+        console.log(
+          'Distribution Submissions from last round: ',
+          keys,
+          values,
+          size,
+        );
+        let isValid;
+        const submitterAccountKeyPair = await this.getSubmitterAccount();
+        const submitterPubkey = submitterAccountKeyPair.publicKey.toBase58();
 
-      for (let i = 0; i < size; i++) {
-        let candidatePublicKey = keys[i];
-        console.log('FOR CANDIDATE KEY', candidatePublicKey);
-        let candidateKeyPairPublicKey = new PublicKey(keys[i]);
-        if (candidatePublicKey == submitterPubkey) {
-          console.log('YOU CANNOT VOTE ON YOUR OWN DISTRIBUTION SUBMISSIONS');
-        } else {
-          try {
-            console.log(
-              'DISTRIBUTION SUBMISSION VALUE TO CHECK',
-              values[i].submission_value,
-            );
-            isValid = await validateDistribution(
-              values[i].submission_value,
-              round,
-            );
-            console.log(`Voting ${isValid} to ${candidatePublicKey}`);
-
-            if (isValid) {
-              // check for the submissions_audit_trigger , if it exists then vote true on that otherwise do nothing
-              const distributions_audit_trigger =
-                taskAccountDataJSON.distributions_audit_trigger[round];
+        for (let i = 0; i < size; i++) {
+          let candidatePublicKey = keys[i];
+          console.log('FOR CANDIDATE KEY', candidatePublicKey);
+          let candidateKeyPairPublicKey = new PublicKey(keys[i]);
+          if (candidatePublicKey == submitterPubkey) {
+            console.log('YOU CANNOT VOTE ON YOUR OWN DISTRIBUTION SUBMISSIONS');
+          } else {
               console.log(
-                'SUBMIT DISTRIBUTION AUDIT TRIGGER',
-                distributions_audit_trigger,
+                'DISTRIBUTION SUBMISSION VALUE TO CHECK',
+                values[i].submission_value,
               );
-              // console.log(
-              //   "CANDIDATE PUBKEY CHECK IN AUDIT TRIGGER",
-              //   distributions_audit_trigger[candidatePublicKey]
-              // );
-              if (
-                distributions_audit_trigger &&
-                distributions_audit_trigger[candidatePublicKey]
-              ) {
-                console.log('VOTING TRUE ON DISTRIBUTION AUDIT');
+              isValid = await validateDistribution(
+                values[i].submission_value,
+                round,
+              );
+              console.log(`Voting ${isValid} to ${candidatePublicKey}`);
+
+              if (isValid) {
+                // check for the submissions_audit_trigger , if it exists then vote true on that otherwise do nothing
+                const distributions_audit_trigger =
+                  taskAccountDataJSON.distributions_audit_trigger[round];
+                console.log(
+                  'SUBMIT DISTRIBUTION AUDIT TRIGGER',
+                  distributions_audit_trigger,
+                );
+                // console.log(
+                //   "CANDIDATE PUBKEY CHECK IN AUDIT TRIGGER",
+                //   distributions_audit_trigger[candidatePublicKey]
+                // );
+                if (
+                  distributions_audit_trigger &&
+                  distributions_audit_trigger[candidatePublicKey]
+                ) {
+                  console.log('VOTING TRUE ON DISTRIBUTION AUDIT');
+                  const response = await this.distributionListAuditSubmission(
+                    candidateKeyPairPublicKey,
+                    isValid,
+                    submitterAccountKeyPair,
+                    round,
+                  );
+                  console.log(
+                    'RESPONSE FROM DISTRIBUTION AUDIT FUNCTION',
+                    response,
+                  );
+                }
+              } else if (isValid == false) {
+                // Call auditSubmission function and isValid is passed as false
+                console.log('RAISING AUDIT / VOTING FALSE ON DISTRIBUTION');
                 const response = await this.distributionListAuditSubmission(
                   candidateKeyPairPublicKey,
                   isValid,
@@ -791,25 +804,11 @@ class NamespaceWrapper {
                   response,
                 );
               }
-            } else if (isValid == false) {
-              // Call auditSubmission function and isValid is passed as false
-              console.log('RAISING AUDIT / VOTING FALSE ON DISTRIBUTION');
-              const response = await this.distributionListAuditSubmission(
-                candidateKeyPairPublicKey,
-                isValid,
-                submitterAccountKeyPair,
-                round,
-              );
-              console.log(
-                'RESPONSE FROM DISTRIBUTION AUDIT FUNCTION',
-                response,
-              );
-            }
-          } catch (err) {
-            console.log('ERROR IN ELSE CONDITION FOR DISTRIBUTION', err);
           }
         }
       }
+    } catch (err) {
+      console.error(err);
     }
   }
   async getTaskLevelDBPath() {
