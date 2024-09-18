f4f3e3982741efedc8ec230764279bc27c09c972
Laura Abro <laura@koii.network>
2024-09-18 12:30:32 -0300

    fix incorrect path

--- src/task/0-setup.js
+++ src/task/0-setup.js
@@ -1,5 +1,5 @@
 import { app } from '@_koii/namespace-wrapper';
-import { setupRoutes } from './routes.js';
+import { setupRoutes } from '../routes.js';
 
 export async function setup() {
   // define any steps that must be executed before the task starts
