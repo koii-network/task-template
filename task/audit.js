218c3996437a0e2166d92d6add80d8caea6fc80f
Herman Liang <herman@koii.network>
2024-09-25 17:02:19 +0000

    @feature/metadata validate node new ns wrapper

---
+++ task/audit.js
@@ -0,0 +1,30 @@
+const { namespaceWrapper } = require('@_koii/namespace-wrapper');
+
+class Audit {
+  /**
+   * Validates the submission value by your logic
+   *
+   * @param {string} submission_value - The submission value to be validated
+   * @param {number} round - The current round number
+   * @returns {Promise<boolean>} The validation result, return true if the submission is correct, false otherwise
+   */
+  async validateNode(submission_value, round, submitter_pubkey) {
+    // Write Your Validation Logic Here
+    console.log(`VALIDATE NODE FOR ROUND ${round} FOR SUBMITTER ${submitter_pubkey}`);
+    return submission_value === 'Hello, World!';
+  }
+  /**
+   * Vote on the other nodes Submissions
+   *
+   * @param {number} roundNumber - The current round number
+   * @returns {void}
+   */
+  async auditTask(roundNumber) {
+    await namespaceWrapper.validateAndVoteOnNodes(
+      this.validateNode,
+      roundNumber,
+    );
+  }
+}
+const audit = new Audit();
+module.exports = { audit };
