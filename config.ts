5bee395f02b19c28271a417b83ad160d96941eb8
Laura Abro <laura@koii.network>
2024-10-01 15:16:06 -0300

    move non-sensitive config out of .env

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const TASK_ID = 'BXbYKFdXZhQgEaMFbeShaisQBYG1FD4MiSf9gg4n6mVn'; //Easy Testing Task ID
+export const TEST_KEYWORDS = ['TEST', 'EZ TESTING'];
+export const WEBPACKED_FILE = '../dist/main.js';
