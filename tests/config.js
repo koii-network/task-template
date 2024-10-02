45457a6428315b6e73af3e5eb264c621bb3b139e
Laura Abro <laura@koii.network>
2024-10-02 13:46:32 -0300

    fix keywords logic

--- tests/config.js
+++ tests/config.js
@@ -4,7 +4,10 @@ export const TASK_ID =
   process.env.TASK_ID || "BXbYKFdXZhQgEaMFbeShaisQBYG1FD4MiSf9gg4n6mVn";
 export const WEBPACKED_FILE_PATH =
   process.env.WEBPACKED_FILE_PATH || "../dist/main.js";
-export const TEST_KEYWORDS = (process.env.TEST_KEYWORDS ?? "").split(",") || [
+
+  const envKeywords = process.env.TEST_KEYWORDS ?? "";
+
+export const TEST_KEYWORDS = envKeywords? envKeywords.split(",") : [
   "TEST",
   "EZ TESTING",
 ];
