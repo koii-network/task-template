52c3b3bd07675d8b2bba7d2278425239073fa538
Laura Abro <laura@koii.network>
2024-10-08 08:36:33 -0300

    add comments to prevent invalid distribution lists

--- src/task/4-distribution.js
+++ src/task/4-distribution.js
@@ -5,6 +5,10 @@ export function distribution(submitters, bounty, roundNumber) {
    * Generate the reward list for a given round
    * This function should return an object with the public keys of the submitters as keys
    * and the reward amount as values
+   *
+   * IMPORTANT: If the slashedStake or reward is not an integer, the distribution list will be rejected
+   * Values are in ROE, or the KPL equivalent (1 Token = 10^9 ROE)
+   *
    */
   console.log(`MAKE DISTRIBUTION LIST FOR ROUND ${roundNumber}`);
   const distributionList = {};
@@ -17,19 +21,19 @@ export function distribution(submitters, bounty, roundNumber) {
     } else if (submitter.votes < 0) {
       const slashedStake = submitter.stake * SLASH_PERCENT;
       distributionList[submitter.publicKey] = -slashedStake;
-      console.log('CANDIDATE STAKE SLASHED', submitter.publicKey, slashedStake);
+      console.log("CANDIDATE STAKE SLASHED", submitter.publicKey, slashedStake);
     } else {
       approvedSubmitters.push(submitter.publicKey);
     }
   }
   if (approvedSubmitters.length === 0) {
-    console.log('NO NODES TO REWARD');
+    console.log("NO NODES TO REWARD");
     return distributionList;
   }
   // reward the submitters who submitted correct values
   const reward = Math.floor(bounty / approvedSubmitters.length);
-  console.log('REWARD PER NODE', reward);
-  approvedSubmitters.forEach(candidate => {
+  console.log("REWARD PER NODE", reward);
+  approvedSubmitters.forEach((candidate) => {
     distributionList[candidate] = reward;
   });
   return distributionList;
