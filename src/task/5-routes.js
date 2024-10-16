e67f0f49afc16010f91407d44b4df37664ff3b3b
Laura Abro <laura@koii.network>
2024-10-16 14:38:33 -0300

    move routes file to make it more visible

--- src/task/5-routes.js
+++ src/task/5-routes.js
@@ -2,6 +2,13 @@ import { namespaceWrapper } from "@_koii/namespace-wrapper";
 
 export function setupRoutes(app) {
   if (app) {
+    /**
+     *
+     * Define all your custom routes here
+     *
+     */
+
+    // Example route
     app.get("/value", async (_req, res) => {
       const value = await namespaceWrapper.storeGet("value");
       console.log("value", value);
