108dfccefac70db5c079d5383f4d5d7bef18a63d
Laura Abro <laura@koii.network>
2024-10-16 14:39:12 -0300

    add explanatory comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import { initializeTaskManager } from "@_koii/task-manager";
+// any custom setup logic you need
 import { setup } from "./task/0-setup.js";
+
+// your task, submission, and audit logic
 import { task } from "./task/1-task.js";
 import { submission } from "./task/2-submission.js";
 import { audit } from "./task/3-audit.js";
+
+// rewards calculation
 import { distribution } from "./task/4-distribution.js";
-import { routes } from "./routes.js";
+
+// custom REST API routes
+import { routes } from "./task/5-routes.js";
 
 initializeTaskManager({
   setup,
