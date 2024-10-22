455013e7017c78d85c3866d238bf28fe578349fd
Laura Abro <laura@koii.network>
2024-10-22 12:20:37 +0000

    Update audit function

--- src/task/3-audit.js
+++ src/task/3-audit.js
@@ -1,8 +1,10 @@
-export async function audit(submission, roundNumber) {
+import { namespaceWrapper } from "@_koii/namespace-wrapper";
+
+export async function audit(submission, roundNumber, submitterKey) {
   /**
    * Audit a submission
    * This function should return true if the submission is correct, false otherwise
    */
-  console.log(`AUDIT SUBMISSION FOR ROUND ${roundNumber}`);
+  console.log(`AUDIT SUBMISSION FOR ROUND ${roundNumber} from ${submitterKey}`);
   return submission === "Hello, World!";
 }
