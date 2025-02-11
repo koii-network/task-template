9b8df54ad01ccac4c2cc07dee7e21567f473e51f
herman124410 <herman@koii.network>
2025-02-11 10:00:47 -0400

    works but shown errors in code  ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import DotenvWebpack from "dotenv-webpack";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,16 +12,31 @@ export default {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     libraryTarget: "commonjs2",
+    clean: true
   },
   target: "node",
+  
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
   },
+  
+  plugins: [
+    new DotenvWebpack()
+  ],
   devtool: "source-map",
 };
